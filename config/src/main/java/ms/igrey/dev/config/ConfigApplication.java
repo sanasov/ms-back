0ca0827f43be7f179ef5d548c364050299c71e53
sanasov <igrey@mail.ru>
2018-12-09 17:43:09 +0300

    gateway

--- config/src/main/java/ms/igrey/dev/config/ConfigApplication.java
+++ config/src/main/java/ms/igrey/dev/config/ConfigApplication.java
@@ -1,4 +1,4 @@
-package com.piggymetrics.config;
+package ms.igrey.dev.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
